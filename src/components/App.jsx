57397741634aa86515ec7ddf22ce87b8f8075b29
KorotkovK <korotkov.kostya123@gmail.com>
2023-11-26 22:18:12 +0200

    main

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { AppWrap, TitleApp, TitleContactList } from './App.styled';
+import { Toaster } from 'react-hot-toast';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <AppWrap>
+      <TitleApp>Phonebook</TitleApp>
+      <ContactForm />
+      <TitleContactList>Contacts</TitleContactList>
+      <Filter />
+      <ContactList />
+      <Toaster />
+    </AppWrap>
   );
 };
